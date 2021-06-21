979522becaf9484e713dbf601d06c470a90d927c
Tobias Schneider <mail@tobias-schneider.io>
2021-06-21 02:44:32 +0200

    Typescript rewrite

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,44 @@
 class Base64URL {
-    static parse(s) {
-        return new Uint8Array(Array.prototype.map.call(atob(s.replace(/-/g, '+').replace(/_/g, '/').replace(/\s/g, '')), c => c.charCodeAt(0)))
+    static parse(s: string): Uint8Array {
+        return new Uint8Array(Array.from(atob(s.replace(/-/g, '+').replace(/_/g, '/').replace(/\s/g, ''))).map(c => c.charCodeAt(0)))
     }
-    static stringify(a) {
-        return btoa(String.fromCharCode.apply(0, a)).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
+    static stringify(a: Uint8Array): string {
+        return btoa(String.fromCharCode.apply(0, Array.from(a))).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
     }
 }
 
+interface JWTPayload {
+    iat: number,
+    nbf: number,
+    exp: number
+}
+
 class JWT {
-    constructor() {
-        if (!crypto || !crypto.subtle)
-            throw new Error('Crypto not supported!')
-        this.algorithms = {
-            HS256: {
-                name: 'HMAC',
-                hash: {
-                    name: 'SHA-256'
-                }
-            },
-            HS512: {
-                name: 'HMAC',
-                hash: {
-                    name: 'SHA-512'
-                }
+    protected algorithms = {
+        HS256: {
+            name: 'HMAC',
+            hash: {
+                name: 'SHA-256'
+            }
+        },
+        HS512: {
+            name: 'HMAC',
+            hash: {
+                name: 'SHA-512'
             }
         }
     }
-    _utf8ToUint8Array(str) {
+
+    constructor() {
+        if (!crypto || crypto.subtle)
+            throw new Error('Crypto not supported!')
+    }
+
+    protected _utf8ToUint8Array(str: string): Uint8Array {
         return Base64URL.parse(btoa(unescape(encodeURIComponent(str))))
     }
-    _decodePayload(raw) {
+
+    protected _decodePayload(raw: string): JWTPayload | null {
         switch (raw.length % 4) {
             case 0:
                 break
@@ -48,7 +57,8 @@ class JWT {
             return null
         }
     }
-    async sign(payload, secret, algorithm = 'HS256') {
+
+    public async sign(payload: JWTPayload, secret: string, algorithm: 'HS256' | 'HS512' = 'HS256') {
         if (payload === null || typeof payload !== 'object')
             throw new Error('payload must be an object')
         if (typeof secret !== 'string')
@@ -58,7 +68,8 @@ class JWT {
         const importAlgorithm = this.algorithms[algorithm]
         if (!importAlgorithm)
             throw new Error('algorithm not found')
-        payload.iat = Math.floor(Date.now() / 1000)
+        if (!payload.iat)
+            payload.iat = Math.floor(Date.now() / 1000)
         const payloadAsJSON = JSON.stringify(payload)
         const partialToken = `${Base64URL.stringify(this._utf8ToUint8Array(JSON.stringify({ alg: algorithm, typ: 'JWT' })))}.${Base64URL.stringify(this._utf8ToUint8Array(payloadAsJSON))}`
         const key = await crypto.subtle.importKey('raw', this._utf8ToUint8Array(secret), importAlgorithm, false, ['sign'])
@@ -74,7 +85,8 @@ class JWT {
         const signature = await crypto.subtle.sign(importAlgorithm.name, key, this._utf8ToUint8Array(partialToken))
         return `${partialToken}.${Base64URL.stringify(new Uint8Array(signature))}`
     }
-    async verify(token, secret, algorithm = 'HS256') {
+
+    public async verify(token: string, secret: string, algorithm: 'HS256' | 'HS512' = 'HS256') {
         if (typeof token !== 'string')
             throw new Error('token must be a string')
         if (typeof secret !== 'string')
@@ -91,6 +103,8 @@ class JWT {
         const key = await crypto.subtle.importKey('raw', keyData, importAlgorithm, false, ['sign'])
         const partialToken = tokenParts.slice(0, 2).join('.')
         const payload = this._decodePayload(tokenParts[1].replace(/-/g, '+').replace(/_/g, '/'))
+        if (!payload)
+            return false
         if (payload.nbf && payload.nbf >= Math.floor(Date.now() / 1000))
             return false
         if (payload.exp && payload.exp < Math.floor(Date.now() / 1000))
@@ -100,7 +114,8 @@ class JWT {
         const res = await crypto.subtle.sign(importAlgorithm.name, key, messageAsUint8Array)
         return Base64URL.stringify(new Uint8Array(res)) === signaturePart
     }
-    decode(token) {
+
+    public decode(token: string): JWTPayload | null {
         return this._decodePayload(token.split('.')[1].replace(/-/g, '+').replace(/_/g, '/'))
     }
 }
